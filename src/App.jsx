0de681828b0321b4a83fedd5d0591f304b4e627c
blanq31 <obiwulugodswill@gmail.com>
2023-07-22 12:18:55 +0100

    to-do list completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,49 @@
 import Header from './Header';
+import SearchItem from './SearchItem';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-        id: 1,
-        checked: true,
-        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-    },
-    {
-        id: 2,
-        checked: false,
-        item: "Item 2"
-    },
-    {
-        id: 3,
-        checked: false,
-        item: "Item 3"
-    }
-]);
+const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
 
+const [newItem, setNewItem] = useState('');
+
+const [search, setSearch] = useState('');
+
+const setAndSaveItems = (newItems) => {
+  setItems(newItems);
+  localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+}
+
+//adds a new item
+const addItem = (item) => {
+  const id = items.length ? items[items.length - 1].id + 1 : 1;
+  const myNewItem = {id, checked: false, item };
+  const listItems = [...items, myNewItem];
+  setAndSaveItems(listItems);
+}
+
+// handles the checking of the checkbox
 const handleCheck = (id) => {
   const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked }: item);
-  setItems(listItems);
-  localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  setAndSaveItems(listItems);
 }
 //onChange hook is used for input element while onClick can be used for buttons and other elements
-
+//handles the deletion  of an item
 const handleDelete = (id) => {
   const listItems = items.filter((item) => item.id !== id);//creates a new array that items whose id is not equal to the id passed in
-  setItems(listItems);
-  localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  setAndSaveItems(listItems);
+}
+
+//handles the submission of a new list
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if (!newItem) return;
+  //addItem
+  addItem(newItem);
+  setNewItem(''); 
 }
 
 
@@ -41,12 +52,21 @@ const handleDelete = (id) => {
     <>
       <div className="App">
         <Header title="Grocery List" />
+        <AddItem 
+          newItem={newItem}
+          setNewItem={setNewItem}
+          handleSubmit={handleSubmit}
+        />
+        <SearchItem 
+          search={search}
+          setSearch={setSearch}
+        />
         <Content 
-          items={items}
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
           handleCheck={handleCheck}
           handleDelete={handleDelete}
         />
-        <Footer />
+        <Footer length={items.length} />
       </div>
     </>
   )
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,35 +1,19 @@
-import { FaTrashAlt } from 'react-icons/fa'
+import ItemList from './ItemList';
 
 const Content = ({ items, handleCheck, handleDelete }) => {
    
  
   return (
     <main>
-        {items.length ? (<ul>
-            {items.map((item) => (
-                <li className='item' key={item.id}>
-                    <input 
-                    type="checkbox"
-                    onChange={() => handleCheck(item.id)}
-                    checked={item.checked}
-                    />
-                    <label
-                    onDoubleClick={() => handleCheck(item.id)}
-                    style={(item.checked) ? { textDecoration: 'line-through' } : null}
-                    >{item.item}</label>
-                    <FaTrashAlt 
-                    onClick={() => handleDelete(item.id)}
-                    role='button' 
-                    tabIndex='0'
-                    />
-                </li>
-            ))}
-        </ul> 
+        {items.length ? (
+            <ItemList 
+                items={items}
+                handleCheck={handleCheck}
+                handleDelete={handleDelete}
+            />
         ) : (
             <p style={{ marginTop: '2rem'}}> Your list is empty</p>
         )}
-        
-       
     </main>
   )
 }
